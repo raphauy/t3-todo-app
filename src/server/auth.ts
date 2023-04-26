c3230ebc2fc65badac04ef76c4191e5c3240e835
raphauy <rapha.uy@rapha.uy>
2023-04-26 12:03:32 -0300

    first commit

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -49,7 +49,7 @@ export const authOptions: NextAuthOptions = {
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER || 'http://localhost:3000',
-        port: 465,
+        port: 587,
         auth: {
           user: process.env.EMAIL_FROM,
           pass: process.env.EMAIL_PASSWORD || "",
